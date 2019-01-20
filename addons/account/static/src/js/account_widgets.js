f7220b84dd75878daf90228247309115fccc0850
Hans Henrik Gabelgaard <hhg@gabelgaard.org>
2019-01-20 16:47:22 +0100

    Issue15002: Don't destroy tax field, only hide

--- addons/account/static/src/js/account_widgets.js
+++ addons/account/static/src/js/account_widgets.js
@@ -949,7 +949,7 @@ openerp.account = function (instance) {
             var hideGroupResponseClosureFactory = function(field_widget, $container, obj_key){
                 return function(has_group){
                     if (has_group) $container.show();
-                    else {
+                    else if (obj_key != 'tax_id_field') {
                         field_widget.destroy();
                         $container.remove();
                         delete self[obj_key];
